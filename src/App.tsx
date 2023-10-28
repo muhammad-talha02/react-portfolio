3ee1f02b101af02f24e99b21027086d7cfbe97b3
Muhammad Talha Saeed <104321361+muhammad-talha02@users.noreply.github.com>
2023-10-28 12:48:41 +0000

    Working on framer motion

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,17 @@
 import "./App.scss"
 import { Hero, Navbar } from "./components"
+import Test from "./components/test/Test"
 const App = () => {
   return (
-    <div>
-      <section>
-      <Navbar/>
-      </section>
-      <Hero/>
-      <Hero/>
-      <Hero/>
-    </div>
+    // <div>
+    //   <section>
+    //   <Navbar/>
+    //   </section>
+    //   <Hero/>
+    //   <Hero/>
+    //   <Hero/>
+    // </div>
+    <Test/>
   )
 }
 
--- src/components/test/Test.tsx
+++ src/components/test/Test.tsx
@@ -1,7 +1,15 @@
+import { motion } from "framer-motion"
+import "./test.scss"
 const Test = () => {
   return (
-    <div>
-      
+    <div className="course">
+      <motion.div className="Box" 
+      initial={{opacity:0, scale:0.5}}
+      animate={{
+        opacity:1 , scale:1
+      }} transition={{duration:2, delay:0.5}}>
+
+      </motion.div>
     </div>
   )
 }
