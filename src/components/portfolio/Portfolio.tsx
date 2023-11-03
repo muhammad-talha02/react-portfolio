d626c4374ed969a759b3dec74d890b3ffd48eaa1
Muhammad Talha Saeed <104321361+muhammad-talha02@users.noreply.github.com>
2023-11-03 05:42:53 +0000

    Porjects section Added

--- src/components/portfolio/Portfolio.tsx
+++ src/components/portfolio/Portfolio.tsx
@@ -1,4 +1,4 @@
-import {motion, useScroll , useSpring} from "framer-motion"
+import { motion, useScroll, useSpring, useTransform } from "framer-motion"
 import { Projects } from "../../utils"
 import "./portfolio.scss"
 import { useRef } from "react"
@@ -11,28 +11,47 @@ type SInglePorjectProps = {
     }
 }
 const SingleProject = ({ item }: SInglePorjectProps) => {
+
+    const ref = useRef()
+    const { scrollYProgress } = useScroll({
+        target: ref,
+        // offset: ["start start", "end start"]
+    })
+
+    const y = useTransform(scrollYProgress, [0, 1], [-300, 300])
     return (
-        <section>
-            {item.title}
+        <section >
+            <div className="container">
+                <div className="wrapper">
+                    <div className="imageContainer" ref={ref}>
+                        <img src={item.img} alt="" />
+                    </div>
+                    <motion.div className="textContainer" style={{ y: y }}>
+                        <motion.h2>{item.title}</motion.h2>
+                        <p>{item.desc}</p>
+                        <button>See Demo</button>
+                    </motion.div>
+                </div>
+            </div>
         </section>
     )
 }
 
 const Portfolio = () => {
-const ref = useRef()
-    const {scrollYProgress} = useScroll({
-        target:ref,
-        offset:["end end", "start start"]
+    const ref = useRef()
+    const { scrollYProgress } = useScroll<any>({
+        target: ref,
+        offset: ["end end", "start start"]
     })
 
     const scaleX = useSpring(scrollYProgress, {
-        stiffness:100, damping:30
+        stiffness: 100, damping: 30
     })
     return (
         <div ref={ref} className="portfolio">
             <div className="progress">
                 <h1>Featured Projects</h1>
-                <motion.div style={{scaleX:scaleX}} className="progressBar"></motion.div>
+                <motion.div style={{ scaleX: scaleX }} className="progressBar"></motion.div>
             </div>
 
             {
