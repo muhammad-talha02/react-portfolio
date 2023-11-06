3f4443fb3a6e1a1401dea4c94f9c26c34b59615f
Muhammad Talha Saeed <104321361+muhammad-talha02@users.noreply.github.com>
2023-11-06 12:47:29 +0000

    Fixed Responsive issue sin contact sectiuon

--- src/App.tsx
+++ src/App.tsx
@@ -25,9 +25,9 @@ const App = () => {
           <Parallex />
         </section> */}
         <Portfolio />
-        {/* <section id="contact">
+        <section id="contact">
           <Contact />
-        </section> */}
+        </section>
         {/* <Hero/> */}
         {/* <Hero/> */}
       </div>
